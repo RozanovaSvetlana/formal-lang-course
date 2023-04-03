23c984b3d7ca838c9efa1fd33c4767e6281fff06
Vasya <khitrik.rozanova.svetlana@gmail.com>
2023-04-03 23:22:52 +0300

    feat: translation of context-free grammar into weak Chomsky's normal form

--- project/context_free_grammar.py
+++ project/context_free_grammar.py
@@ -1,6 +1,25 @@
 from pyformlang.cfg import CFG, Variable
 
 
+def cfg_into_weak_cnf(cfg, start_symbol=Variable("S")) -> CFG:
+    """
+        Translation of context-free grammar
+        into weak Chomsky's normal form
+    :param cfg: context-free grammar as object or as a string
+    :param start_symbol: start symbol, S by default, for cfg as a string
+    :return: contex free grammar into wcnf
+    """
+    if isinstance(cfg, str):
+        cfg = CFG.from_text(cfg, start_symbol)
+    cfg = (
+        cfg.remove_useless_symbols()
+        .eliminate_unit_productions()
+        .remove_useless_symbols()
+    )
+    cfg = cfg._decompose_productions(cfg._get_productions_with_only_single_terminals())
+    return CFG(start_symbol=cfg.start_symbol, productions=set(cfg))
+
+
 def get_cfg_from_file(file_name: str, start_symbol=Variable("S")) -> CFG:
     """
         Get context free grammar from file
